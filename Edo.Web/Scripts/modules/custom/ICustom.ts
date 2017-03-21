7348fa825c7a388584d7dac29f991cd3065339a7
liuluosheng <346642458@qq.com>
2017-03-21 17:35:05 +0800

    自定义视图脚本实现

---
+++ Edo.Web/Scripts/modules/custom/ICustom.ts
@@ -0,0 +1,12 @@
+﻿/*
+ * 自定义View中的Controller继承此类
+ * 步骤： 1. 在Entity实体类中为要创建自定义的属性添加View属性。
+ *           2. 创建相应的视图。
+ *           3.在Scripts\Modules\custom目录下创建相应的TypeScript模块，模块名字规则为：类名+属性名  ，全部小写。如User类中的Permissions属性命名为：userpermissions
+ *              模块实现一个控制器，自定义功能在其中定义。并继承此接口。
+ *           4. 在视图中使用控制器名称为：模块名称+Ctrl
+ */
+interface ICoustom {
+    $inject: string[];
+    constructor;
+} 
---
+++ Edo.Web/Scripts/modules/custom/userpermissions.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="icustom.ts" />
+import angular = require("angular");
+class UserPermissions implements ICoustom {
+    $inject = ["$scope"];
+    constructor($scope) {
+        let app = angular.module("app");
+        $scope.test = 123322;
+    }
+}
+export = UserPermissions;
